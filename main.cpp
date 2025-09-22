ee0bc5f9afb9c2919c85e2f558fc2c30367772c5
L B <lbryant019@insite.4cd.edu>
2025-09-21 19:30:07 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*Comsc 210 | Lab 12 | Lawrence Bryant
+IDE used: VSC*/
+
+#include <iostream>
+#include <algorithm>  // for sort(), find()
+#include <numeric>    // for accumulate()
+#include <array>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("Test_Data");
+    if(fin.good())
+    {
+        
+    }
+}
