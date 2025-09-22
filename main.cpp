1d06c56ad334fb0e2f220952cf8fc59411271223
L B <lbryant019@insite.4cd.edu>
2025-09-21 21:15:12 -0700

    40 minutes.

--- main.cpp
+++ main.cpp
@@ -2,17 +2,21 @@
 IDE used: VSC*/
 
 #include <iostream>
-#include <algorithm>  // for sort(), find()
-#include <numeric>    // for accumulate()
+#include <algorithm>  
+#include <numeric>    
 #include <array>
 #include <fstream>
 #include <string>
 using namespace std;
 
+//The number of entries
 const int SIZE = 30;
 
+//The program main
 int main()
 {
+    //Not sure what ifstream fin actually does. Fin.open selects to file to choose from
+    //Handles array creation
     ifstream fin;
     fin.open("Test_Data.txt");
     string feed = "Fail";
@@ -24,6 +28,7 @@ int main()
         {
             getline(fin,feed);
             fullList[i] = feed;
+            cout << fullList[i] << "\n";
         }
         
         fin.close( );         
@@ -32,5 +37,14 @@ int main()
     {
         cout << "File not found.\n";
     }
+
+    string hunt = "pen";
+    array<string, SIZE>::iterator finder;
+    finder = find(fullList.begin(), fullList.end(), hunt);
+    cout << "The word " << hunt << " was found\n";
+
+    cout << "The array is " << fullList.size() << " elements long";
+    cout << "Element 5 is " << fullList[4] << "\n";
+    cout << "The front and back elements are: " << fullList.front() << " and " << fullList.back() << "\n";
     
 }
